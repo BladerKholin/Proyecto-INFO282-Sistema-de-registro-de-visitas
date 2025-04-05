f5ba69b06f909a8f91334d4449a93f92618dd8ed
BenrriXWorks <BenrriXWorks@gmail.com>
2025-04-05 03:01:49 -0300

    Automated tester subsystem added

---
+++ app_tester/TestCases/TestCaseInterface.py
@@ -0,0 +1,59 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Consider practices: Showing screenshot on failure, logging, and handling exceptions.
+"""For each test, consider saving into an array:
+    results.append({
+        'input': test_case['input'],
+        'expected': test_case['expected'],
+        'actual': actual_text,
+        'success': is_success
+    })
+"""
+
+def wait_and_find(driver, by, value, timeout=10):
+    """Wait for an element to be present and then find it"""
+    try:
+        WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((by, value))
+        )
+        return driver.find_element(by, value)
+    except Exception as e:
+        print(f"Element not found with locator {by}: {value}")
+        print(f"Current page source: {driver.page_source}")
+        raise e
+
+
+class TestCaseInterface:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def run(self): # If fails, it shouls raise and print the test data in that test iteration.
+        raise NotImplementedError("Subclasses should implement run method")
+    
+    def setup(self):
+        raise NotImplementedError("Subclasses should implement setup method")
+
+    def teardown(self):
+        raise NotImplementedError("Subclasses should implement teardown method")
+    
+    def print_results(self, results):
+        for i, result in enumerate(results):
+            print(f"Test {result['id']}:")
+            print(f"  Input: {result['input']}")
+            print(f"  Expected: {result['expected']}")
+            print(f"  Actual: {result['actual']}")
+            print(f"  Success: {result['success']}")
+        
+        total_passed = sum(1 for result in results if result['success'])
+        print(f"Total passed: {total_passed}")
+        
+
+        print(
+            "OK!" if total_passed == len(results) else
+            f"Failed [id]: {[ r["id"] for r in results if not r["success"]]}"
+        )
+
+
+    def wait_and_find(self, by, value, timeout=5):
+        return wait_and_find(self.driver, by, value, timeout)
---
+++ app_tester/main.py
@@ -0,0 +1,79 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+### Options
+options = UiAutomator2Options()
+options.platform_name = 'Android'
+options.device_name = 'emulator-5554'
+options.app = os.getenv('APK_PATH')
+options.automation_name = 'UiAutomator2'
+options.new_command_timeout = 350
+
+
+### Test cases
+from TestCases.TestTextboxMayus.TestTexboxMayus import TestTexboxMayus
+tests = [TestTexboxMayus]
+
+### Main
+def main():
+    
+    ### Load environment variables
+    
+    WEBDRIVER_REMOTE_URL = os.getenv('WEBDRIVER_REMOTE_URL')
+    if WEBDRIVER_REMOTE_URL is None:
+        print("WEBDRIVER_REMOTE_URL not found in environment variables")
+        return
+    
+    ### Driver
+    print("Starting driver session...")
+    driver = webdriver.Remote(WEBDRIVER_REMOTE_URL, options=options)
+    print("Driver session started successfully")
+    
+    for test_case in tests:
+
+        correct = True
+        begin_time = time.time()
+        test = test_case(driver)
+        print("\n------------------------------------------------------")
+        print(f"Test name: {test.test_name}")
+        print(f"Description: {test.description}")
+        print("-----------------------------------------------------")
+        
+        try: 
+            test.setup()
+        except Exception as e:
+            correct = False
+            print(f"Test case setup failed: {test_case.__name__}, Error: {e}")
+            print("SKIPPING TEST CASE")
+            continue
+        
+        try: 
+            results = test.run()
+            test.print_results(results)
+        except Exception as e:
+            correct = False
+            print(f"Test case execution failed: {test_case.__name__}, Error: {e}")
+            print("TEST CASE EXECUTION FAILED")
+
+        try: 
+            test.teardown()
+        except Exception as e:
+            correct = False 
+            print(f"Test case teardown failed: {test_case.__name__}, Error: {e}")
+            print("TEARDOWN FAILED")
+        
+        test_duration = time.time() - begin_time
+        print(f"Test case duration: {test_duration:.2f} seconds || STATUS: {'Execution complete' if correct else 'Execution failed'}")
+        print("-----------------------------------------------------\n")
+
+
+if __name__ == "__main__":
+    main()
